f291adb633b13f1f997af0770ade42389ab829ce
Leonardo Barros <nickolback@gmail.com>
2015-12-15 16:23:05 -0200

    Exemplo de app em rails

---
+++ docker_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DockerExample::Application.config.secret_token = '88f2fb79666c89b00e076243c9a2f7ea1c5384b690094aa9b5e66da8af50750d5be10c27bcf9f8cd049eab5c5d1a2994dfe7303be62c40e4832be3442d4f1b7c'
