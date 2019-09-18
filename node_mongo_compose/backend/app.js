3db9cbeaec3d72fe4fba99cb40beaa161d7a5a31
Leonardo Sales <lsales@corp.globo.com>
2019-09-18 09:37:17 -0300

    Urgent Commit from heaven

--- node_mongo_compose/backend/app.js
+++ node_mongo_compose/backend/app.js
@@ -2,14 +2,28 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
-//Test
+//Middlewares
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
 
-server.get('/', (req, res, next) => res.send('Backend'))
+//ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true }
+})
 
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Routes
+Client.register(server, '/clients')
 // start server
 server.listen(3000)
